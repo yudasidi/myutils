7cfc6e58e4de3ec358bbfbe0b211430e577348b0
yudasidi <yudasidi@gmail.com>
2022-09-21 21:23:04 +0300

    Version 0.3

---
+++ escr_print.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+altozip=$1
+if [ ! -f "$altozip" ]; then
+    echo "$altozip does not exist."
+    exit 1
+fi
+cd $HOME/escr/in
+rm *.xml
+
+cd $HOME/escr/in
+unzip $altozip
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Cannot unzip"
+    exit $retVal
+fi
+python3 $HOME/ladino-transcript/scripts/escr_print.py \$HOME/escr/in \$HOME/escr/out/escr.txt
