6f6a6de8c20464f576998beb240d52def95bb392
yudasidi <yudasidi@gmail.com>
2022-09-21 20:43:01 +0300

    Version 0.1

---
+++ z.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+altozip=$1
+if [ ! -f "$altozip" ]; then
+    echo "$altozip does not exist."
+    exit 1
+fi
+cd $HOME/escr/in
+rm *.xml
+cd $HOME/escr/out
+rm *.xml
+
+
+cd $HOME/escr/in
+unzip $altozip
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Cannot unzip"
+    exit $retVal
+fi
+python3 $HOME/ladino-transcript/scripts/escr_update.py \$HOME/escr/in \$HOME/escr/out
+
+rm /mnt/c/Temp/escr.zip
+cd $HOME/escr/out
+zip /mnt/c/Temp/escr.zip *.xml
